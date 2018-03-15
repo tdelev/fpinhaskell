4234855a89886c8d85f6245598781a3f8a80d9c1
Tomche Delev <tomce.delev@gmail.com>
2018-03-15 22:45:47 +0100

    Initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for fpinhaskell1
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,44 @@
+module Main where
+
+import Lib
+import Text.Printf
+
+main :: IO ()
+main = putStr "Hello Haskell\n"
+
+abs :: Int -> Int
+abs x 
+    | x < 0 = 0 - x
+    | otherwise = x
+
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+format :: String -> Int -> (Int -> Int) -> String
+format msg x f = printf msg x (f x)
+
+formatAbs :: Int -> String
+formatAbs x = format "%d absolute is %d" x Main.abs
+
+find :: [a] -> (a -> Bool) -> Int
+find [] _ = 0
+find (head:cons) pred 
+    | pred head = 1 + find cons pred
+    | otherwise = find cons pred
+    -- then 1 + find xs pred
+    -- else find xs pred
+
+
+isSorted :: [a] -> (a -> a -> Bool) -> Bool
+isSorted [] _ = True
+isSorted [head] _ = True
+isSorted (first:(second:cons)) pred
+    | pred first second = isSorted (second:cons) pred
+    | otherwise = False
